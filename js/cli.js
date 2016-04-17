12432f6be56b514672abdf9d211d5909861a8390
David Lopez <Dave42493@gmail.com>
2016-04-16 21:27:56 -0400

    add prompt package

--- js/cli.js
+++ js/cli.js
@@ -1,6 +1,6 @@
 var pingPong= require('./ping-pong.js').pingPong;
 
-console.log('ping-ponging uo to 100');
+var goal = prompt('What number would you like me to ping-pong up to')
 var result = pingPong(100);
 result.forEach(function(element){
   console.log(element);
